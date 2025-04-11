057194b2ebf6544b3b4d627c6d25877768951f3e
Sirhc <ccharles199220@gmail.com>
2025-04-10 22:53:37 -0400

    Update ui.lua

--- holy_paladin_pvp/ui/ui.lua
+++ holy_paladin_pvp/ui/ui.lua
@@ -7,6 +7,8 @@ function M.create_menu(ui_elements)
     menu_elements.enable_script_check = ui_elements:checkbox("Enable Script", true)
     menu_elements.allow_auto_wings = ui_elements:checkbox("Allow Auto Wings", true)
     menu_elements.wog_threshold = ui_elements:slider_float("WOG HP Threshold", 0.3, 1.0, 0.75, "%.0f%%")
+    menu_elements.enable_fake_cast = ui_elements:checkbox("Enable Fake Casts", true)
+    menu_elements.enable_auto_hoj = ui_elements:checkbox("Auto HoJ Kill Chains", true)
 end
 
 M.menu_elements = menu_elements
