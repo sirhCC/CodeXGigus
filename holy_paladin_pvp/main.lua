0967df139c42938ed71137aba513fd1d98677e60
Sirhc <ccharles199220@gmail.com>
2025-04-10 23:07:49 -0400

    Update main.lua

--- holy_paladin_pvp/main.lua
+++ holy_paladin_pvp/main.lua
@@ -1,21 +1,21 @@
-local plugin = ...
-
 local ui = require("ui")
 local rotation = require("rotation")
 
-function plugin.on_load()
-    print("[Holy Paladin] Loaded.")
-end
+return {
+    on_load = function()
+        print("[Holy Paladin] Loaded.")
+    end,
 
-function plugin.on_unload()
-    print("[Holy Paladin] Unloaded.")
-end
+    on_unload = function()
+        print("[Holy Paladin] Unloaded.")
+    end,
 
-function plugin.run()
-    if not ui.menu_elements.enable_script_check or not ui.menu_elements.enable_script_check:get_state() then return end
-    rotation.run()
-end
+    run = function()
+        if not ui.menu_elements.enable_script_check or not ui.menu_elements.enable_script_check:get_state() then return end
+        rotation.run()
+    end,
 
-function plugin.on_render()
-    -- future visual hooks
-end
+    on_render = function()
+        -- future HUD rendering goes here
+    end
+}
