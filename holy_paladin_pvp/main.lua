8f55d80cb16ceb8824b9e39efa3c3ed6b378781e
Sirhc <ccharles199220@gmail.com>
2025-04-10 22:45:55 -0400

    Update main.lua

--- holy_paladin_pvp/main.lua
+++ holy_paladin_pvp/main.lua
@@ -1,5 +1,21 @@
-local utility = require("utility")
-local healing = require("abilities/healing")
-local beacon = require("abilities/beacon")
+local plugin = ...
 
--- Main script logic here
+local ui = require("ui")
+local rotation = require("rotation")
+
+function plugin.on_load()
+    print("[Holy Paladin] Loaded.")
+end
+
+function plugin.on_unload()
+    print("[Holy Paladin] Unloaded.")
+end
+
+function plugin.run()
+    if not ui.menu_elements.enable_script_check or not ui.menu_elements.enable_script_check:get_state() then return end
+    rotation.run()
+end
+
+function plugin.on_render()
+    -- future visual hooks
+end
