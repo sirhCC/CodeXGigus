26d6817778bf4d24ea6c13021d5000eac604b077
Sirhc <ccharles199220@gmail.com>
2025-04-11 04:12:56 -0400

    Update menu.lua

--- holy_paladin_pvp/menu.lua
+++ holy_paladin_pvp/menu.lua
@@ -1,30 +1,63 @@
+local core = _G.core
+local menu = core.menu
+
 local menu_elements = {
-    main_tree = core.menu.tree_node(),
-    enable_script_check = core.menu.checkbox(true, "enable_script_check"),
-    enable_toggle = core.menu.keybind(7, true, "enable_toggle"),
+    main_tree = menu.tree_node(),
+    enable_script_check = menu.checkbox(true, "enable_script_check"),
+    enable_toggle = menu.keybind(7, true, "enable_toggle"),
 
     pvp_settings = {
-        tree = core.menu.tree_node(),
-        enable_arena = core.menu.checkbox(true, "enable_arena"),
-        enable_bg = core.menu.checkbox(true, "enable_bg"),
-        enable_world_pvp = core.menu.checkbox(true, "enable_world_pvp"),
-        enable_duels = core.menu.checkbox(true, "enable_duels")
+        tree = menu.tree_node(),
+        enable_arena = menu.checkbox(true, "enable_arena"),
+        enable_bg = menu.checkbox(true, "enable_bg"),
+        enable_world_pvp = menu.checkbox(true, "enable_world_pvp"),
+        enable_duels = menu.checkbox(true, "enable_duels")
     },
 
     healing = {
-        tree = core.menu.tree_node(),
-        triage_mode = core.menu.checkbox(false, "triage_mode"),
-        wog_threshold = core.menu.slider_int(10, 100, 75, "wog_threshold")
+        tree = menu.tree_node(),
+        triage_mode = menu.checkbox(false, "triage_mode"),
+        wog_threshold = menu.slider_int(10, 100, 75, "wog_threshold")
     },
 
     utility = {
-        tree = core.menu.tree_node(),
-        auto_wings = core.menu.checkbox(true, "auto_wings"),
-        fake_casts = core.menu.checkbox(true, "fake_casts"),
-        auto_hoj_chain = core.menu.checkbox(true, "auto_hoj_chain"),
-        enable_freedom_prediction = core.menu.checkbox(true, "freedom_prediction"),
-        debug_logs = core.menu.checkbox(false, "debug_logs")
+        tree = menu.tree_node(),
+        auto_wings = menu.checkbox(true, "auto_wings"),
+        fake_casts = menu.checkbox(true, "fake_casts"),
+        auto_hoj_chain = menu.checkbox(true, "auto_hoj_chain"),
+        enable_freedom_prediction = menu.checkbox(true, "freedom_prediction"),
+        debug_logs = menu.checkbox(false, "debug_logs")
     }
 }
+
+-- Register the UI tree rendering
+core.register_on_render_menu_callback(function()
+    menu_elements.main_tree:render("Holy Paladin PvP", function()
+        menu_elements.enable_script_check:render("Enable Script", "Toggle script functionality")
+        menu_elements.enable_toggle:render("Toggle Script", "Keybind toggle")
+
+        if not menu_elements.enable_script_check:get_state() then return end
+
+        menu_elements.pvp_settings.tree:render("PvP Settings", function()
+            menu_elements.pvp_settings.enable_arena:render("Enable in Arena")
+            menu_elements.pvp_settings.enable_bg:render("Enable in Battlegrounds")
+            menu_elements.pvp_settings.enable_world_pvp:render("Enable in World PvP")
+            menu_elements.pvp_settings.enable_duels:render("Enable in Duels")
+        end)
+
+        menu_elements.healing.tree:render("Healing Logic", function()
+            menu_elements.healing.triage_mode:render("Enable Triage Mode")
+            menu_elements.healing.wog_threshold:render("WOG Threshold")
+        end)
+
+        menu_elements.utility.tree:render("Utility & Toggles", function()
+            menu_elements.utility.auto_wings:render("Auto Wings")
+            menu_elements.utility.fake_casts:render("Fake Casts")
+            menu_elements.utility.auto_hoj_chain:render("Auto HoJ Chains")
+            menu_elements.utility.enable_freedom_prediction:render("Freedom Prediction")
+            menu_elements.utility.debug_logs:render("Debug Logging")
+        end)
+    end)
+end)
 
 return menu_elements
